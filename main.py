8f15025c4eafa2a22f114a8cad619c83ec9d6138
pain <ravi.ameer@gmail.com>
2025-09-22 09:53:17 +0530

    final env setup touches

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 def main():
     print("Hello from learn-langchain!")
+    print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
